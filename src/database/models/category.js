3cb22b3b2e4f7a14a5d0e723c335a6d4a95fd6ff
Gustavo Pospi <gutxander@gmail.com>
2022-08-06 21:47:33 -0300

    feat: requisito 7 feito.

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,5 +1,11 @@
 const Category = (sequelize, DataTypes) => {
   const Category = sequelize.define("Category", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     name: DataTypes.STRING,
   },
     {
